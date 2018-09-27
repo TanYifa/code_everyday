d972a445c3c19351c72b3ee8963a79e025370668
TanYifa <36765206+TanYifa@users.noreply.github.com>
2018-09-27 12:23:03 +0800

    Add question 2

--- problems_level_1.py
+++ problems_level_1.py
@@ -11,7 +11,7 @@
 #return [0, 1].
 
  
-class Solution(object):
+class Solution_q1(object):
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -25,4 +25,51 @@ class Solution(object):
                 if residual[k] == nums[i]:
                     return [i,k]
         return None
+       
+#Given a 32-bit signed integer, reverse digits of an integer.
+
+#Example 1:
+
+#Input: 123
+#Output: 321
+#Example 2:
+
+#Input: -123
+#Output: -321
+#Example 3:
+
+#Input: 120
+#Output: 21
+#Note:
+#Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
         
+class Solution_q2(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        negative = False
+        list_x = list(str(x))
+        if list_x[0] == '-':
+            negative = True
+            list_x = list_x[1:]
+
+        while(len(list_x) > 1 and list_x[-1] == '0'):
+            list_x.pop()
+
+        N = len(list_x)
+        for i in range(round(N/2)):
+            list_x[i], list_x[N-i-1] =  list_x[N-i-1],  list_x[i]
+
+        result = ''
+        for _ in list_x:
+            result += _
+        result = int(result)
+
+        if negative:
+            result = -1*result
+        if result > 2147483647 or result < -2147483648:
+            return 0
+        else:
+            return result
